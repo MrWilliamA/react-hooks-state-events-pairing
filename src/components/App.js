7962d13fd5847523feeae4d565af1717a594a141
MrWilliamA <will.archer@hotmail.com>
2022-05-19 15:04:44 +1000

    fin

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,14 @@
 import video from "../data/video.js";
+import Buttons from "./Buttons.js";
+import Video from "./Video.js";
 
 function App() {
   console.log("Here's your data:", video);
-
   return (
-    <div className="App">
-      <iframe
-        width="919"
-        height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        frameborder="0"
-        allowfullscreen
-        title="Thinking in React"
-      />
-    </div>
+    <>
+      <Video videoObject={video} />
+      <Buttons videoObject={video} />
+    </>
   );
 }
 
